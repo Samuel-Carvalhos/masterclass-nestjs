9acd72abf525c010012962e5945664791114aaa9
Samuel de Carvalho <carvalhos.samuel86@gmail.com>
2023-12-09 12:19:36 -0300

    Criei a pasta database e alterei alguns arquivos

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-
-@Controller()
+import { Controller, Get, Body } from '@nestjs/common';
+import { PrismaService } from './database/prisma.service';
+import { randomUUID } from 'crypto';
+import { CreateMembersBody } from './database/dtos/create.members';
+@Controller('app')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private prisma: PrismaService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('hello')
+  async getHello(@Body() body: CreateMembersBody) {
+    const { name, function: memberFunction } = body;
+
+    const member = await this.prisma.members.create({
+      data: {
+        id: randomUUID(),
+        name,
+        function: memberFunction,
+      },
+    });
+
+    return {
+      member,
+    };
   }
 }
---
+++ src/database/dtos/create.members.ts
@@ -0,0 +1,4 @@
+export class CreateMembersBody {
+    name: string;
+    function: string;
+}
