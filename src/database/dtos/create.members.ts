4a23c0573a002346d41172657459b47710ab5428
Samuel de Carvalho <carvalhos.samuel86@gmail.com>
2023-12-11 18:26:05 -0300

    Finalizando projeto

--- src/database/dtos/create.members.ts
+++ src/database/dtos/create.members.ts
@@ -1,4 +1,14 @@
+import { IsNotEmpty, Length } from 'class-validator';
+
 export class CreateMembersBody {
-    name: string;
-    function: string;
+  @IsNotEmpty()
+  @Length(5, 100, {
+    message: 'The member name should be between 5 and 100 characters.',
+  })
+  name: string;
+
+  @IsNotEmpty({
+    message: 'The member function should not be empty.',
+  })
+  function: string;
 }
